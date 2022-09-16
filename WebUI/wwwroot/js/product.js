a7ddbc8b2bc2d0fce65a5446aec0bb43245ddf7e
onrdrmn <onrdrmn@gmail.com>
2022-09-16 04:46:55 +0300

    Edit Button problem solved

--- WebUI/wwwroot/js/product.js
+++ WebUI/wwwroot/js/product.js
@@ -19,15 +19,15 @@ function loadDataTable() {
                 "data": "id",
                 "render": function (data) {
                     return `
-                        <div class="w-10 btn-group" role="group">
-                            <a href="/Admin/Product/Upsert?id=${data} class="btn btn-primary mx-2">
-                            <i class="bi bi-pencil-square"></i>Edit</a>
+                        <div class="w-5 btn-group" role="group">
+                            <a href="/Admin/Product/Upsert?id=${data}"
+                            class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i> Edit</a>
                             <a onClick=Delete('/Admin/Product/Delete/${data}')
-                            class="btn btn-danger mx-2"><i class="bi bi-trash-fill"></i>Delete</a>
-                        </div>
+                            class="btn btn-danger mx-2"><i class="bi bi-trash-fill"></i> Delete</a>
+                        </div> 
                           `
                 },
-                "width": "15%"
+                "width": "20%"
             }
         ]
     });
